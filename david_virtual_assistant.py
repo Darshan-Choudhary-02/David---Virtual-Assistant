47be7775a832e5d6dbac8c0114aa793db3e2b431
Darshan Choudhary <darshanchoudhary2007@gmail.com>
2024-06-12 14:55:53 +0530

    fixed some issues of custom functions

--- david_virtual_assistant.py
+++ david_virtual_assistant.py
@@ -6,8 +6,6 @@ from playsound import playsound
 import datetime
 from config import gemini_api_key
 import google.generativeai as genai
-import time
-
 
 conversation = []
 
@@ -16,8 +14,7 @@ conversation = []
 
 def chat(prompt):
     global conversation
-    conversation.append({"role" : "Darshan" , "content" : prompt})
-    print(f"Role : Darshan \nContent : {prompt}")
+    # print(f"Role : User \nContent : {prompt}")
     
     genai.configure(api_key=gemini_api_key)
 
@@ -30,9 +27,9 @@ def chat(prompt):
     }
 
     model = genai.GenerativeModel(
-        model_name="gemini-1.5-flash",
+        model_name="gemini-1.5-pro",
         generation_config=generation_config,
-        system_instruction=f"you are jarvis, a virtual assistant. you are provided with conversation history {conversation} and your work is to go through the history and analyze it. and if your input prompt is available in history then answer the input from conversation history and if the question is new to you then answer how you normally answer. answers should be to the point and short."
+        system_instruction=f"you are david, a virtual assistant. you are provided with conversation history {conversation} and your work is to go through the history and analyze it. and if your input prompt is available in history then answer the input from conversation history and if the question is new to you then answer how you normally answer. answers should be to the point and short."
     )
 
     chat_session = model.start_chat(history = [])
@@ -40,13 +37,9 @@ def chat(prompt):
     response = chat_session.send_message(prompt)
     response.resolve()
     
-    conversation.append({"role" : "Jarvis","content" : response.text})
-    
-    if "history".lower() in prompt.lower():
-        return chat_session.history
-    
-    if "clear conversation history".lower() in prompt.lower():
-        conversation = []
+    conversation.append({"role" : "user" , "content" : prompt})
+    conversation.append({"role" : "david","content" : response.text})
+  
     return response.text
     
 
@@ -70,11 +63,42 @@ def gemini_ai(prompt):
 
     chat = model.start_chat(history = [])
 
+    
     response = chat.send_message(prompt)
+    
 
     return (response.text)
 
+def conversation_appender(prompt, response = ""):
+    conversation.append({"role" : "user" , "content" : prompt})
+    conversation.append({"role" : "david","content" : response})
+
+def classifier(command):
+    prompt = f"classify the following statement in category of 'Command' or 'question' or 'normal' and return just that one word : {command}"
+    result = gemini_ai(prompt)
+    # print(result)
+    return result.lower()
+
+def new_classifier(command):
+    prompt = f"go through {basic_category} and based on it classify the following statement in category of 'other' or 'normal' and return just that one word : {command}"
+    result = gemini_ai(prompt)
+    # print(result)
+    return result.lower()
+
+def cleaner(code_response):
+    
+    '''removes python word and other symbols from code response'''
+    
+    code_response1 = code_response.replace("python","")
+    final_code_response = code_response1.replace("```","")
+    return final_code_response
+    
+    
+
 def summarizer(history):
+    
+    '''summarizes whole history'''
+    
     summary = gemini_ai(f"summarize the following text : {history}")
     return summary
 
@@ -96,55 +120,88 @@ def takeCommand():
 
             return query
         except Exception as e:
-            print(e)
-            say("Some error occurred by Jarvis A I")
+            # print(e)
+            say("Some error occurred by David A I")
 
+
+basic_category = {
+    "other" : ["open text file", "open specific app/file" ,"create a new text file with name", "find specific folder", "give path of specific     file"] , 
+    "normal" : ["remember something" , "recall it"]}
 
 if __name__ == "__main__":
-    say("Hello I am Jarvis A I")
+    say("Hello I am David A I")
     sites = [["Youtube","https://www.youtube.com"],["wikipedia","https://www.wikipedia.org"],["google","https://google.com"]]
     while True:
         print("listening..")
-        command = takeCommand()
-        # print(f"Query : {command}")
-        try:
-            for site in sites:
-                if f"open {site[0]}".lower() in command.lower():
-                    say(f"opening {site[0]} Sir")
-                    print(f"opening {site[0]} Sir")
-                    webbrowser.open(site[1])
+        try: 
+            command = takeCommand().lower()
+            print(f"User : {command}")
 
-            if "quit" in command.lower():
-                break
+            if "quit" in command :
+                exit()
+                
+            if command=="david":
+                continue
             
-            elif "open music" in command.lower():
-                musicPath = "C:/Users/acer/Downloads/sample_music.mp3"
-                # playsound(musicPath) #alternate way
-                os.system(f"start {musicPath}")
-
-            elif "time" in command.lower():
-                hour = datetime.datetime.now().strftime("%H")
-                mins = datetime.datetime.now().strftime("%M")
-                say(f"Current time is {hour} hours {mins} minutes")
-
-            elif "search".lower() in command.lower():
-                base_url = "https://www.google.com/search?q="
-                search_item = command.split("search",1)[1] # splits the command once after word "search" and returns words after search
-                final_search_item = str('+'.join(search_item.split())) #separates words in search_item with +
-                final_url = base_url+final_search_item
-                webbrowser.open_new(final_url)
-
-            elif "artificial".lower() in command.lower():
+            category = classifier(command)
+            
+            if "command".lower() in category:
+                print("Working on it Sir.")
+                say("Working on it Sir.")
+                
+                new_category = new_classifier(command)
+                # print(new_category)
+                               
+                if "open music" in command :
+                    musicPath = "C:/Users/acer/Downloads/sample_music.mp3"
+                    # playsound(musicPath) #alternate way
+                    os.system(f"start {musicPath}")
+                
+                elif "clear conversation history".lower() in command:
+                    conversation = []  
+                    
+                elif "conversation history".lower() in command :
+                    # print("here")
+                    print(conversation)
+                                
+                elif "other".lower() in new_category:
+                    new_prompt = f"just return the code which will fulfill the command , just return the code of following command and nothing else : {command} consider that the device is of Windows OS"
+                    code_response = gemini_ai(new_prompt)
+                    final_code = cleaner(code_response)
+                    conversation_appender(command,final_code)
+                    # print(final_code)
+                    exec(final_code)
+                    
+                else:
+                    chat_response = chat(command)
+                    conversation_appender(command,chat_response)
+                    print(f"Role : David\nContent : {chat_response}")
+                    say(chat_response)
+                   
+            elif "question".lower() in category:
+                
+                if "time" in command :
+                    hour = datetime.datetime.now().strftime("%H")
+                    mins = datetime.datetime.now().strftime("%M")
+                    say(f"Current time is {hour} hours {mins} minutes")
+                    
+                else:
+                    chat_response = chat(command)
+                    conversation_appender(command,chat_response)
+                    print(f"Role : David\nContent : {chat_response}")
+                    say(chat_response)
+                        
+                  
+            elif "artificial".lower() in command :
                 prompt = command.split("intelligence",1)[1]
                 response = gemini_ai(prompt)
+                conversation_appender(command,chat_response)
                 print(response)
-            
-            elif "conversation history".lower() in command.lower():
-                    chat_history = chat(command)
-                    
+              
             else:
                 chat_response = chat(command)
-                print(f"Role : Jarvis\nContent : {chat_response}")
+                conversation_appender(command,chat_response)
+                print(f"Role : David\nContent : {chat_response}")
                 say(chat_response)
                 
                 
